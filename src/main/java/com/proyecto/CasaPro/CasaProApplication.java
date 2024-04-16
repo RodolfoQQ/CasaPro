612930aa6262872f55c0adfe26691835e579be15
Rod007kn2000 L <i202021234@cibertec.edu.pe>
2024-04-15 19:28:50 -0500

    subiendo proyecto

---
+++ src/main/java/com/proyecto/CasaPro/CasaProApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.CasaPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/CasaPro/CasaProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.CasaPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
